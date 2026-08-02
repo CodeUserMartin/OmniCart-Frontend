021ae7309d05c214a3b78efe8d6ed538f5a23600
CodeUserMartin <martinpatole33@gmail.com>
2026-08-02 20:13:48 +0530

    minor api changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import  axios  from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
+    baseURL: import.meta.env.VITE_API_URL, // Add your Backend API URL here or Localhost URL
     withCredentials: true,
 })
 
